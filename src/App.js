1feffc18cf79401f3ad09735ba27451823b3f672
Christopher P <54770918+weekendatchewie@users.noreply.github.com>
2019-10-10 18:25:14 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Contact from './components/Contact.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Contact avatar = "https://randomuser.me/api/portraits/men/25.jpg" name = "John Connor" isOnline />
+
+      <Contact avatar = "https://randomuser.me/api/portraits/women/34.jpg" name = "Sarah Connor" isOnline  />
+
+      <Contact avatar = "https://randomuser.me/api/portraits/men/3.jpg" name = "Chico Lopez"  />
+
+
     </div>
   );
 }
